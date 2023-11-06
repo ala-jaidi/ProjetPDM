6262cd678dfcf2593c170b52676aeb609c9254ff
G.Issam <guezmir.issam@esprit.tn>
2023-11-06 19:34:49 +0100

    initial commit

---
+++ ProjetMobile/ProjetMobileApp.swift
@@ -0,0 +1,20 @@
+//
+//  ProjetMobileApp.swift
+//  ProjetMobile
+//
+//  Created by IssamGuezmir on 6/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjetMobileApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
