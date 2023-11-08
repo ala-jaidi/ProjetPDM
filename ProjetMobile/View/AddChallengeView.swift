9c9103e1523d6cf2d6e62454524b1a827ca44a6c
manel zaabi <manel.zaabi@esprit.tn>
2023-11-08 19:34:13 +0100

    Details Event

---
+++ ProjetMobile/View/AddChallengeView.swift
@@ -0,0 +1,100 @@
+//
+//  AddChallengeView.swift
+//  ProjetMobile
+//
+//  Created by manel zaabi on 7/11/2023.
+//
+
+import SwiftUI
+
+
+struct AddChallengeView: View {
+    @State private var title = ""
+        @State private var description = ""
+        @State private var date = Date()
+        @State private var selectedImage: Image?
+       @State private var showImagePicker = false
+    var body: some View {
+        NavigationView {
+                   Form {
+                       Section(header: Text("Informations du défi")) {
+                           TextField("Titre", text: $title)
+                           TextField("Description", text: $description)
+                           DatePicker("Date", selection: $date, in: Date()...)
+                     Text("Image du défi")
+                                          if selectedImage != nil {
+                                              selectedImage?
+                                                  .resizable()
+                                                  .scaledToFit()
+                                                  .frame(height: 150) // Ajustez la taille de l'aperçu selon vos besoins
+                                          }
+                                          Button("Sélectionner une image") {
+                                              self.showImagePicker = true
+                                          }
+                                          .sheet(isPresented: $showImagePicker) {
+                                              ImagePicker(selectedImage: $selectedImage)
+                                          }
+                                      }
+                       
+                       Button(action: {
+                           // Ajoutez ici la logique pour enregistrer le défi dans la base de données
+                       }) {
+                           Text("Enregistrer")
+                           
+                               
+                       }
+                           NavigationLink(destination: ChallengeView()) {
+                               Text("Consulter liste des defis")
+                                   .font(.title3)
+                                   .padding()
+                                   .background(Color.green)
+                                   .foregroundColor(.white)
+                                   .cornerRadius(20)
+                           }
+                       
+                           
+
+                   }
+            
+                   .navigationTitle("Ajouter un défi")
+            
+               }
+    }
+}
+
+struct AddChallengeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddChallengeView()
+    }
+}
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var selectedImage: Image?
+    @Environment(\.presentationMode) var presentationMode
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.selectedImage = Image(uiImage: uiImage)
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
---
+++ ProjetMobile/View/ChallengeView.swift
@@ -0,0 +1,274 @@
+//
+//  ChallengeView.swift
+//  ProjetMobile
+//
+//  Created by manel zaabi on 7/11/2023.
+//
+
+import SwiftUI
+
+struct ChallengeView: View {
+    var body: some View {
+        NavigationView {
+            
+            ZStack() {
+                Rectangle()
+                    .frame(width: 350, height: 210)
+                    .foregroundColor(.clear)
+                    .background(Image("Rectangle 33"))
+                    .cornerRadius(16)
+                    .offset(x: 0, y: -256)
+                
+                Text("Top Challenges")
+                    .font(Font.custom("Poppins", size: 24).weight(.semibold))
+                    .foregroundColor(Color(red: 0.12, green: 0.12, blue: 0.12))
+                    .offset(x: -67, y: -108)
+                Button(action: {
+                    // Ajoutez votre action de participation à l'événement ici
+                    print("Participation à l'événement")
+                }) {
+                    ZStack() {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 130, height: 30)
+                        
+                            .background(Color(red: 1, green: 1, blue: 1).opacity(10))
+                            .cornerRadius(14)
+                            .offset(x: 0, y: 0)
+                        
+                        Text("Join us!")
+                            .font(Font.custom("Poppins", size: 20).weight(.semibold))
+                            .foregroundColor(Color(red: 0, green: 4, blue: 0).opacity(15))
+                        
+                            .offset(x: 0, y: 0)
+                        ZStack() {
+                            // Contenu de la superposition interne (le cas échéant)
+                        }
+                    }
+                }  
+                .frame(width: 126, height: 34)
+                .offset(x: -97, y: -186)
+                .opacity(0.50)
+                Button(action: {
+                    // Ajoutez votre logique de participation à l'événement ici
+                    print("Participation à l'événement")
+                }) {
+                    ZStack() {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 164, height: 185)
+                            .background(Color(red: 0.95, green: 0.95, blue: 0.95))
+                            .cornerRadius(16)
+                            .offset(x: 0, y: 0)
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 134, height: 100)
+                            .background(
+                                Image("Rectangle 56"))
+                            .cornerRadius(12)
+                            .offset(x: 0, y: -27.50)
+                        NavigationLink(destination: DetailsEvent()) {
+                            Text("Eco-rotic")
+                                .font(Font.custom("Poppins", size: 14).weight(.medium))
+                                .foregroundColor(.black)
+                                .offset(x: -0.50, y: 41)
+                        }
+                        
+                        
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 65, height: 21)
+                            .background(Color(red: 0.44, green: 0.88, blue: 0))
+                            .cornerRadius(6)
+                            .offset(x: -0.50, y: 67)
+                        
+                        Text("Join")
+                            .font(Font.custom("Poppins", size: 10).weight(.medium))
+                            .foregroundColor(.white)
+                            .offset(x: 0, y: 67)
+                    }
+                }
+                .frame(width: 164, height: 185)
+                .offset(x: -92, y: 32.50)
+                
+                Button(action: {
+                    // Ajoutez votre logique de participation à l'événement ici
+                    print("Join")
+                }) {
+                    ZStack() {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 164, height: 185)
+                            .background(Color(red: 0.95, green: 0.95, blue: 0.95))
+                            .cornerRadius(16)
+                            .offset(x: 0, y: 0)
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 134, height: 100)
+                            .background(
+                                Image("Rectangle 57"))
+                            .cornerRadius(12)
+                            .offset(x: 0, y: -27.50)
+                        NavigationLink(destination: DetailsEvent()) {
+                            Text("Green wear")
+                                .font(Font.custom("Poppins", size: 14).weight(.medium))
+                                .foregroundColor(.black)
+                                .offset(x: -1, y: 41)
+                        }
+                       
+                        
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 65, height: 21)
+                            .background(Color(red: 0.44, green: 0.88, blue: 0))
+                            .cornerRadius(6)
+                            .offset(x: -0.50, y: 67)
+                        
+                        Text("Join")
+                            .font(Font.custom("Poppins", size: 10).weight(.medium))
+                            .foregroundColor(.white)
+                            .offset(x: 0, y: 67)
+                    }
+                }
+                .frame(width: 164, height: 185)
+                .offset(x: 92, y: 32.50)
+                
+                Button(action: {
+                    // Ajoutez votre logique de participation à l'événement ici
+                    print("Join")
+                }) {
+                    ZStack() {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 164, height: 185)
+                            .background(Color(red: 0.95, green: 0.95, blue: 0.95))
+                            .cornerRadius(16)
+                            .offset(x: 0, y: 0)
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 134, height: 100)
+                            .background(
+                                Image("Rectangle 56-2"))
+                            .cornerRadius(12)
+                            .offset(x: 0, y: -27.50)
+                        NavigationLink(destination: DetailsEvent()) {
+                            Text("On feet")
+                                .font(Font.custom("Poppins", size: 14).weight(.medium))
+                                .foregroundColor(.black)
+                            .offset(x: -0.50, y: 41)}
+                        
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 65, height: 21)
+                            .background(Color(red: 0.44, green: 0.88, blue: 0))
+                            .cornerRadius(6)
+                            .offset(x: -0.50, y: 67)
+                        
+                        Text("Join")
+                            .font(Font.custom("Poppins", size: 10).weight(.medium))
+                            .foregroundColor(.white)
+                            .offset(x: 0, y: 67)
+                    }
+                }
+                .frame(width: 164, height: 185)
+                .offset(x: -93, y: 240.50)
+                
+                Button(action: {
+                    // Ajoutez votre logique de participation à l'événement ici
+                    print("Participation à l'événement")
+                }) {
+                    ZStack() {
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 164, height: 185)
+                            .background(Color(red: 0.95, green: 0.95, blue: 0.95))
+                            .cornerRadius(16)
+                            .offset(x: 0, y: 0)
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 134, height: 100)
+                            .background(
+                                Image("Rectangle 56-3"))
+                            .cornerRadius(12)
+                            .offset(x: 0, y: -27.50)
+                        NavigationLink(destination: DetailsEvent()) {
+                            Text("Eco-friendly")
+                                .font(Font.custom("Poppins", size: 14).weight(.medium))
+                                .foregroundColor(.black)
+                            .offset(x: -1, y: 41)}
+                        
+                        Rectangle()
+                            .foregroundColor(.clear)
+                            .frame(width: 65, height: 21)
+                            .background(Color(red: 0.44, green: 0.88, blue: 0))
+                            .cornerRadius(6)
+                            .offset(x: -0.50, y: 67)
+                        
+                        
+                        Text("Join")
+                            .font(Font.custom("Poppins", size: 10).weight(.medium))
+                            .foregroundColor(.white)
+                            .offset(x: 0, y: 67)
+                    }
+                }
+                .frame(width: 164, height: 185)
+                .offset(x: 91, y: 240.50)
+                
+                ZStack() {
+                    ZStack() {
+                        
+                    }
+                    .frame(width: 30, height: 30)
+                    .offset(x: 139, y: -11)
+                    ZStack() {
+                        
+                    }
+                    .frame(width: 30, height: 30)
+                    .offset(x: 46, y: -11)
+                    ZStack() {
+                        
+                    }
+                    .frame(width: 30, height: 30)
+                    .offset(x: -47, y: -11)
+                    ZStack() {
+                        
+                    }
+                    .frame(width: 30, height: 30)
+                    .offset(x: -140, y: -11)
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: 169.01, height: 0)
+                        .overlay(Rectangle()
+                            .stroke(.black, lineWidth: 2.10))
+                        .offset(x: -0.49, y: 40)
+                    Ellipse()
+                        .foregroundColor(.clear)
+                        .frame(width: 6, height: 6)
+                        .background(Color(red: 0.44, green: 0.88, blue: 0))
+                        .offset(x: 46, y: 7)
+                }
+                .frame(width: 390, height: 100)
+                .background(Color(red: 0.95, green: 0.95, blue: 0.95))
+                .cornerRadius(20)
+                .offset(x: 0, y: 372)
+                .shadow(
+                    color: Color(red: 0, green: 0, blue: 0, opacity: 0.05), radius: 25
+                )
+                
+            }
+            .frame(width: 390, height: 844)
+            .background(.white)
+            .cornerRadius(20)
+           
+            
+        }
+    
+    }
+}
+
+
+struct ChallengeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChallengeView()
+    }
+}
